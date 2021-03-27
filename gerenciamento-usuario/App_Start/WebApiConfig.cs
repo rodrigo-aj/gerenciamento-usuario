c00cb7d95b2fd169b7a1af2bff4385fd6f36b43a
Rodrigo Almeida de Jesus <rodrigo.jesus@powertiss.com.br>
2021-03-27 20:11:19 -0300

    Commit inicial

---
+++ gerenciamento-usuario/App_Start/WebApiConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace gerenciamento_usuario
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApiAction",
+                routeTemplate: "api/{controller}/{action}"
+            );
+        }
+    }
+}
---
+++ gerenciamento-usuario/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="gerenciamento_usuario.WebApiApplication" Language="C#" %>
